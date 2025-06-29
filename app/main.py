83515a55ec846d5e9437621d9f55dc1e3a7463b5
litvi <litvin4uk.roman@gmail.com>
2025-06-29 17:18:04 +0300

    Correction

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ import os
 def copy_file(line: str) -> None:
     command_parts = line.split(" ")
     text = ""
-    if (len(command_parts) < 3 or command_parts[0] != "cp"
-            or command_parts[1] == command_parts[2]):
+    if len(command_parts) < 3 or command_parts[0] != "cp":
+        return
+
+    if command_parts[1] == command_parts[2]:
         return
 
     if not os.path.exists(command_parts[1]):
